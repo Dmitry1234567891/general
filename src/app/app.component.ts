b1fccbc7097605aa7d5618f3c95f1136616f5b4e
Dmitry Kushnir <sendmail.dima@gmai.com>
2018-10-31 22:30:03 +0200

    my2commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  carName:string;
+  colorCar:string;
+  engineCapacity:number;
+  yachtClass:number;
+  state:boolean;
+  stateYacht:string;
+  carVisibleHide:string; // hidden visible List Cars
+  yachtVisibleHide:string;  // hidden visible List Yahts
+
+    cars :{name: string, color: string, enrgcapacity: number }[]=[];
+    yachts:{yclass:number, state: boolean}[]=[];
+
+  constructor(){
+    this.state=false;
+    this.carVisibleHide='none';
+    this.yachtVisibleHide='none';
+
+
+  }
+
+  addcarName(carN:string){
+    this.carName=carN;
+   // this.cars[0].name=carN;
+    //console.log(this.cars[0].name);
+  }
+
+   addcarColor(colorC:string){
+    this.colorCar=colorC;
+  }
+
+  addcarengineCapacity(engineC:number){
+    this.engineCapacity=engineC;
+  }
+
+  addYachtClass(ycl:number){
+    this.yachtClass=ycl;
+
+  }
+
+  addState(st:string){
+    this.stateYacht=st;
+  }
+
+
+  listCarVisibleHide(){
+
+     if(this.carVisibleHide==='none'){
+       this.carVisibleHide='block';
+     }else{
+       this.carVisibleHide='none';
+     }
+
+  }
+
+  listYachtVisibleHide(){
+    if(this.yachtVisibleHide==='none'){
+      this.yachtVisibleHide='block';
+    }else{
+      this.yachtVisibleHide='none';
+    }
+  }
+
+  addProducts(){
+
+    this.cars.push({name: this.carName, color: this.colorCar, enrgcapacity: this.engineCapacity });
+    this.yachts.push({yclass:this.yachtClass, state: this.state});
+
+console.log(':)');
+  }
+  // setData(set:string , data:string){
+  //   this.set=data;
+  // }
 }
