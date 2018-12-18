72278b7f967effa547eca1856a5d654dc6667964
Dmitry1234567891 <44652004+Dmitry1234567891@users.noreply.github.com>
2018-12-18 21:07:46 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
-import { Directive, Renderer2, ElementRef, OnInit } from '@angular/core';
+import { Renderer2, ElementRef, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 testarr:any =['Tomatoes','Cucumbers'];
   displayFruits=true;
   displayBerries=true;
 
-renderer:Renderer2;
-element:ElementRef;
+ //renderer:Renderer2;
+//element:ElementRef;
 
+constructor(private renderer:Renderer2,private element:ElementRef){
+
+}
+
+ngOnInit(){
+
+ // this.renderer.addClass(this.element.nativeElement, 'hiddenElement');
+}
  showHideList(nameElem){
 
-   this.element=nameElem;
-   this.renderer.addClass(this.element.nativeElement, 'hiddenElement');
+    const u =this.renderer.selectRootElement('#vegetables');
+    this.renderer.setStyle(u, 'color', 'red');
+   console.log(this.element.nativeElement);
+
 
 
  }
